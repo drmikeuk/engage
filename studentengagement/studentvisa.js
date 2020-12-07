59f169238a2415ecca0e4f19448b00b8658e6d39
mike <drmikegardner@gmail.com>
2020-12-07 15:17:06 +0000

    redo days filter so all onclick - works

--- studentengagement/studentvisa.js
+++ studentengagement/studentvisa.js
@@ -47,57 +47,41 @@ $(document).ready( function () {
       });
 
 
-      // add cutom range filter for days inactive
-      // https://datatables.net/examples/plug-ins/range_filtering.html
-      $.fn.dataTable.ext.search.push(
-          function( settings, data, dataIndex ) {
-              // grab active radio button value  eg '0-7'
-              range =  $("input[name=days]").filter(":checked").val();
-
-              if (range == "All") {
-                min = 0;
-                max = '1000';
-                console.log ("  reset search ");
-              } else if (range == ">30") {
-                min = 30;
-                max = '1000';
-                console.log ("  search > 30 ");
-              } else {
-                parts = range.split("-");
-                min = parts[0];
-                max = parts[1];
-                console.log ("  search " + min + " to " + max);
-              }
-
-              var days = parseFloat( data[3] ) || 0;    // use data for the days column
-              if ( ( isNaN( min ) && isNaN( max ) ) ||
-                   ( isNaN( min ) && age <= max ) ||
-                   ( min <= days  && isNaN( max ) ) ||
-                   ( min <= days  && days <= max ) )
-              {
-                  return true;
-              }
-              return false;
-          }
-      );
-
 
       // add search action to days filters
+      // https://datatables.net/examples/plug-ins/range_filtering.html
+      // https://stackoverflow.com/questions/18958075/add-range-filter-datatable-feature-to-button-click
       // btngroup = #daysfilters; input ids = range (eg '1-7') or 'All'
       $('#daysfilters').on('click', 'label', function() {
         range = $(this).attr('for');        // eg 1-7
-        if (range != "All"){
+
+        if (range == "All") {
+          min = 0;
+          max = '1000';
+          //console.log ("Reset search (all)");
+        } else if (range == ">30") {
+          min = 30;
+          max = '1000';
+          //console.log ("Search > 30 ");
+        } else {
           parts = range.split("-");
           min = parts[0];
           max = parts[1];
-
-          console.log ("Click filter days..." + min + " to " + max);
-        }
-        else {
-          console.log ("Click filter days reset (ie all)");
+          //console.log ("Search " + min + " to " + max);
         }
 
-        // min max logic is in custom search - just call table redraw here
+        // push to custom filter
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var days = parseFloat( data[3] ) || 0; // use data for the days column
+
+                if (days >= min && days <= max)
+                    return true;
+                return false;
+            }
+        );
+
+        // redraw
         $('#DataTable').DataTable().draw();
       });
 
